2b38181898c84e4d1dd8f00623abfe318900ae10
alephcero <4lephcero@gmail.com>
2017-05-05 16:50:36 -0400

    first draft

---
+++ js/map.js
@@ -0,0 +1,7 @@
+var map = L.map('map',{zoomControl:false}).setView([-34.615889, -58.442420], 12);
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+}).addTo(map);
+
+var geocoder = L.Mapzen.geocoder('mapzen-fkPfGup');
+geocoder.addTo(map);
